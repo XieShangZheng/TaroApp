981c2184dd3d45b3e31b717d4b7f0833e02d5462
AT <734237292@qq.com>
2020-04-02 16:30:34 +0800

    add: 初试 Hooks

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,134 +1,43 @@
-import { ComponentClass } from 'react'
-import Taro, { Component, Config } from '@tarojs/taro'
+import Taro, { useState } from '@tarojs/taro'
 import { View } from '@tarojs/components'
-import { connect } from '@tarojs/redux'
 import { PostCard, PostForm } from '../../components';
-import { add, minus, asyncAdd } from '../../actions/counter'
 
 import './index.scss'
 
-// #region 书写注意
-//
-// 目前 typescript 版本还无法在装饰器模式下将 Props 注入到 Taro.Component 中的 props 属性
-// 需要显示声明 connect 的参数类型并通过 interface 的方式指定 Taro.Component 子类的 props
-// 这样才能完成类型检查和 IDE 的自动提示
-// 使用函数模式则无此限制
-// ref: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/20796
-//
-// #endregion
+export default function Index() {
+  const [posts, setPosts] = useState([
+    {
+      title: '泰罗奥特曼',
+      content: '泰罗是奥特之父和奥特之母唯一的亲生儿子。',
+    },
+  ])
+  const [formTitle, setFormTitle] = useState('');
+  const [formContent, setFormContent] = useState('');
 
-type PageStateProps = {
-  counter: {
-    num: number
-  }
-}
-
-type PageDispatchProps = {
-  add: () => void
-  dec: () => void
-  asyncAdd: () => any
-}
-
-type PageOwnProps = {}
-
-type PageState = {}
-
-type IProps = PageStateProps & PageDispatchProps & PageOwnProps
-
-interface Index {
-  props: IProps
-}
-
-@connect(({ counter }) => ({
-  counter
-}), (dispatch) => ({
-  add() {
-    dispatch(add())
-  },
-  dec() {
-    dispatch(minus())
-  },
-  asyncAdd() {
-    dispatch(asyncAdd())
-  }
-}))
-class Index extends Component {
-  state = {
-    posts: [
-      {
-        title: '泰罗奥特曼',
-        content: '泰罗是奥特之父和奥特之母唯一的亲生儿子。',
-      },
-    ],
-    formTitle: '',
-    formContent: '',
-  }
-  /**
- * 指定config的类型声明为: Taro.Config
- *
- * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
- * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
- * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
- */
-  config: Config = {
-    navigationBarTitleText: '首页'
-  }
-
-  componentWillReceiveProps(nextProps) {
-    console.log(this.props, nextProps)
-  }
-  componentWillUnmount() { }
-  componentDidShow() { }
-  componentDidHide() { }
-
-  handleSubmit(e) {
+  const handleSubmit = e => {
     e.preventDefault()
-
-    const { formTitle: title, formContent: content } = this.state
-    const newPosts = this.state.posts.concat({ title, content })
-
-    this.setState({
-      posts: newPosts,
-      formTitle: '',
-      formContent: '',
-    })
+    const newPosts = posts.concat({ title: formTitle, content: formContent })
+    setPosts(newPosts);
+    setFormTitle('');
+    setFormContent('');
   }
 
-  handleTitleInput(e) {
-    this.setState({
-      formTitle: e.target.value,
-    })
-  }
-
-  handleContentInput(e) {
-    this.setState({
-      formContent: e.target.value,
-    })
-  }
-
-  render() {
-    return (
-      <View className='index'>
-        {this.state.posts.map((post, index) => (
-          <PostCard key={index} title={post.title} content={post.content} />
-        ))}
-        <PostForm
-          formTitle={this.state.formTitle}
-          formContent={this.state.formContent}
-          handleSubmit={e => this.handleSubmit(e)}
-          handleTitleInput={e => this.handleTitleInput(e)}
-          handleContentInput={e => this.handleContentInput(e)}
-        />
-      </View>
-    )
-  }
+  return (
+    <View className='index'>
+      {posts.map((post, index) => (
+        <PostCard key={index} title={post.title} content={post.content} />
+      ))}
+      <PostForm
+        formTitle={formTitle}
+        formContent={formContent}
+        handleSubmit={e => handleSubmit(e)}
+        handleTitleInput={e => setFormTitle(e.target.value)}
+        handleContentInput={e => setFormContent(e.target.value)}
+      />
+    </View>
+  )
 }
 
-// #region 导出注意
-//
-// 经过上面的声明后需要将导出的 Taro.Component 子类修改为子类本身的 props 属性
-// 这样在使用这个子类时 Ts 才不会提示缺少 JSX 类型参数错误
-//
-// #endregion
-
-export default Index as ComponentClass<PageOwnProps, PageState>
+Index.config = {
+  navigationBarTitleText: '首页'
+}
