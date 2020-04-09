60f332f7dc1d5a8f17edb0befd0b04adff6493c6
AT <734237292@qq.com>
2020-04-09 16:40:02 +0800

    update: 重构详情页

--- src/pages/post/post.tsx
+++ src/pages/post/post.tsx
@@ -1,15 +1,26 @@
 import Taro, { useRouter } from '@tarojs/taro'
 import { View } from '@tarojs/components'
+import { useSelector } from '@tarojs/redux';
+import _ from 'lodash'
+
 import { PostCard } from '../../components'
 import './post.scss'
 
+type State = {
+  post: {
+    posts: object
+  }
+}
+
 export default function Post() {
   const router = useRouter();
-  const { params } = router;
+  const { postId } = router.params;
 
+  const posts = useSelector((state: State) => state.post.posts)
+  const post = _.find(posts, ['id', postId])
   return (
     <View className='post'>
-      <PostCard title={params.title} content={params.content}></PostCard>
+      <PostCard post={post}></PostCard>
     </View>
   )
 }
