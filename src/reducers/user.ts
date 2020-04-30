ce376cdeab601de6fefe91fc4e857a80c0e7b60b
AT <734237292@qq.com>
2020-04-30 17:11:42 +0800

    update: 设置用户信息和腿粗action

--- src/reducers/user.ts
+++ src/reducers/user.ts
@@ -1,4 +1,11 @@
-import { SET_LOGIN_INFO, SET_IS_OPENED, LOGIN_SUCCESS, LOGIN, LOGIN_ERROR, LOGIN_NORMAL } from '../constants/';
+import {
+	SET_LOGIN_INFO,
+	LOGIN_SUCCESS,
+	LOGIN,
+	LOGIN_ERROR,
+	LOGIN_NORMAL,
+	SET_LOGIN_OUT,
+} from '../constants/';
 
 const INITIAL_STATE = {
 	userId: '',
@@ -11,16 +18,19 @@ const INITIAL_STATE = {
 
 export default function user(state = INITIAL_STATE, action) {
 	switch (action.type) {
-		case SET_IS_OPENED: {
-			const { isOpened } = action.payload;
-
-			return { ...state, isOpened };
+		case SET_LOGIN_INFO: {
+			return { ...state, ...action.payload };
 		}
 
-		case SET_LOGIN_INFO: {
-			const { avatar, nickName, userId } = action.payload;
-
-			return { ...state, nickName, avatar, userId };
+		case SET_LOGIN_OUT: {
+			return {
+				...state,
+				avatar: '',
+				nickName: '',
+				userId: '',
+				loginStatus: LOGIN_NORMAL,
+				isLogin: false,
+			};
 		}
 
 		case LOGIN: {
