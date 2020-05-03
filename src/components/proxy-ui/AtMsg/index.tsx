e2d6d51417a2d5c9bef91e1389e158ae060734df
AT <734237292@qq.com>
2020-05-03 23:37:06 +0800

    feat(components/proxy-ui/atmsg.tsx): 通过监听全局消息中心,代理 mp-colorui 组件达到与 taro-ui 使用方式一致的目的

---
+++ src/components/proxy-ui/AtMsg/index.tsx
@@ -0,0 +1,33 @@
+import Taro, { useState, useEffect } from "@tarojs/taro";
+import { ClMessage } from 'mp-colorui';
+
+export default function AtMsg() {
+  const [message, setMessage] = useState()
+  const [type, setType] = useState();
+  const [duration, setDuration] = useState(3);
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    Taro.eventCenter.on('atMessage', (options = {}) => {
+      const { message: asMsg, type: asType, duration: asDuration = 3 } = options
+      setShow(true);
+      setType(asType);
+      setMessage(asMsg);
+      setDuration(asDuration)
+    })
+
+    // 绑定函数
+    Taro.atMessage = Taro.eventCenter.trigger.bind(
+      Taro.eventCenter,
+      'atMessage'
+    )
+
+    return () => {
+      Taro.eventCenter.off('atMessage')
+    }
+  })
+
+  return (
+    <ClMessage onClose={() => setShow(false)} message={message} type={type} duration={duration} show={show} />
+  )
+}
