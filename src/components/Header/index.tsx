b2b7fb47a1c64c4154aef03cbfa8bfea81fc6720
AT <734237292@qq.com>
2020-05-03 23:36:02 +0800

    feat(components/header/index.jsx): 移除 taro-ui, 引入 AtMsg 代理 mp-colorui

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,9 @@
 import Taro from '@tarojs/taro'
 import { View } from '@tarojs/components'
-import { AtMessage } from 'taro-ui'
 import { useSelector } from '@tarojs/redux'
 
 import LoggedMine from '../LoggedMine'
+import AtMsg from '../proxy-ui/AtMsg';
 import WeAppLoginButton from '../WeAppLoginButton'
 import './index.scss'
 
@@ -15,13 +15,12 @@ type State = {
 
 export default function Header() {
   const isWeapp = Taro.getEnv() === Taro.ENV_TYPE.WEAPP
-
   const loginStatus = useSelector((state: State) => state.user.loginStatus)
   const isLogged = loginStatus === 'LOGIN_SUCCESS';
 
   return (
     <View className='user-box'>
-      <AtMessage />
+      <AtMsg />
       <LoggedMine />
       {!isLogged && (
         <View className='login-button-box'>
