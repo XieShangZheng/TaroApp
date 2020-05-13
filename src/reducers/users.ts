3e3f95baaf2fe82cf6ca5e4c3f553c463aa592c1
AT <734237292@qq.com>
2020-05-13 16:06:27 +0800

    feat(reducers/users.ts): 获取用户信息

--- src/reducers/users.ts
+++ src/reducers/users.ts
@@ -9,20 +9,19 @@ const INITIAL_STATE = {
 export default function users(state = INITIAL_STATE, action) {
 	switch (action.type) {
 		case GET_USERS: {
-			return { ...state, postStatus: GET_USERS, isUsers: true };
+			return { ...state, usersStatus: GET_USERS, isUsers: true };
 		}
 
 		case UPDATE_USERS: {
-			return { ...state, users: state.users };
+			return { ...state, users: action.payload.users };
 		}
 
 		case USERS_SUCCESS: {
-			return { ...state, postStatus: USERS_SUCCESS, isUsers: false };
+			return { ...state, usersStatus: USERS_SUCCESS, isUsers: false };
 		}
 
 		case SET_USERS: {
-			const { users: asUsers } = action.payload;
-			return { ...state, users: state.users.concat(...asUsers) };
+			return { ...state, users: state.users.concat(...action.payload.users) };
 		}
 
 		default:
