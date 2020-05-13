ebc479bb20a91dab53a5c0976a77c5bfd19c91d0
AT <734237292@qq.com>
2020-05-13 14:18:08 +0800

    feat(users): 用户列表样式
    git push

--- src/pages/users/users.tsx
+++ src/pages/users/users.tsx
@@ -1,6 +1,6 @@
 import Taro, { pxTransform, useEffect, } from '@tarojs/taro'
 import { View } from '@tarojs/components'
-import { ClButton, ClText, ClIcon } from 'mp-colorui'
+import { ClButton, ClText, ClIcon, ClAvatar } from 'mp-colorui'
 import { useDispatch, useSelector } from '@tarojs/redux'
 import { GET_USERS } from '../../constants'
 import './users.scss'
@@ -22,33 +22,41 @@ export default function Users() {
   console.log('%cAT-users: ', 'color: #bf2c9f; background: pink; font-size: 13px;', users);
 
   useEffect(() => {
-    async function getUsers() {
-      try {
-        dispatch({
-          type: GET_USERS,
-        })
-      } catch (err) { console.log('getUsers ERR -> pages/users', err) }
-    }
-    if (!users.length) {
-      getUsers();
-    }
-  }, [users, dispatch])
+    try {
+      dispatch({
+        type: GET_USERS,
+      })
+    } catch (err) { console.log('getUsers ERR -> pages/users', err) }
+  }, [])
+
+  const onImageClick = (avatar) => {
+    Taro.previewImage({
+      urls: [avatar],
+    })
+  }
 
   return (
     <View className='list'>
-      <View className='item'>
-        <View className='content'>
-          <ClText textColor='gray' align='left'>序号：1</ClText>
-          <View style={{ width: pxTransform(500) }}>
-            <ClText textColor='brown' cut align='left'>昵称：泰泰国、新加坡、印度尼西亚泰国、新加坡、印度尼西亚泰国、新加坡、印度尼西亚泰国、新加坡、印度尼西亚国、新加坡、印度尼西亚~ </ClText>
-          </View>
-        </View>
-        <View className='btn'>
-          <ClButton shape='radius' bgColor='grey'>
-            <ClIcon iconName='edit' size='small' color='white' />
-          </ClButton>
-        </View>
-      </View>
+      {
+        users.length && users.map((item, index): any => {
+          return (
+            <View className='item' key={item._id}>
+              <View className='angle'>{index}</View>
+
+              {item.avatar && (
+                <ClAvatar headerArray={[{ url: item.avatar, bgColor: 'green' }]} shape='round' onClick={() => onImageClick(item.avatar)} />
+              )}
+
+              <View className='name' style={{ width: pxTransform(500) }}>
+                <ClText textColor='brown' cut align='left'>{item.nickName}</ClText>
+              </View>
+              <View className='btn'>
+                <ClButton size='small' plain shape='round' shadow bgColor='green' plainSize='bold' >操作</ClButton>
+              </View>
+            </View>
+          )
+        })
+      }
     </View>
   )
 }
