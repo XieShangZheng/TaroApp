72857d5be5b6d42624e4ab2efe6fa7c10c2f1ed9
AT <734237292@qq.com>
2020-05-14 13:48:13 +0800

    feat(roles): 新增用户权限常量表

---
+++ src/constants/roles.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/prefer-default-export
+export const ROLES = [
+	{ value: '0', label: '用户列表', desc: '用户列表入口显示权限' },
+	{ value: '1', label: '用户操作', desc: '操作用户权限' },
+];
