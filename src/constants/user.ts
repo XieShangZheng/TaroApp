e130ffae6bf8f7ab24fe5751adedcd92ff3168f4
AT <734237292@qq.com>
2020-05-14 18:57:45 +0800

    feat(constants/user.ts): 更新用户常量

--- src/constants/user.ts
+++ src/constants/user.ts
@@ -5,3 +5,6 @@ export const LOGIN_SUCCESS = 'LOGIN_SUCCESS' // 更新登录成功信息
 export const LOGIN = 'LOGIN' // 监听登录动作
 export const LOGIN_ERROR = 'LOGIN_ERROR' // 设置登录失败信息
 export const LOGIN_NORMAL = 'LOGIN_NORMAL'
+
+export const UPDATE_USER = 'UPDATE_USER'; // 更新用户信息
+export const UPDATE_SUCCESS = 'UPDATE_SUCCESS'; // 更新状态
