dd9f6c48e8e19416b73b4ff1488f941cbf667151
AT <734237292@qq.com>
2020-05-15 14:25:49 +0800

    feat(users.tsx): 将之前 Operation 组件写回 users 中

--- src/pages/users/users.tsx
+++ src/pages/users/users.tsx
@@ -1,10 +1,12 @@
-import Taro, { pxTransform, useEffect, } from '@tarojs/taro'
+import Taro, { pxTransform, useEffect, useState } from '@tarojs/taro'
 import { View } from '@tarojs/components'
-import { ClText, ClAvatar } from 'mp-colorui'
+import { ClText, ClAvatar, ClModal, ClTitleBar, ClLayout, ClFlex, ClButton } from 'mp-colorui'
 import { useDispatch, useSelector } from '@tarojs/redux'
+import { AtMessage, AtCheckbox } from 'taro-ui'
+import { isEqual, isEmpty } from 'lodash'
 import { ReactNode } from 'react'
-import { Operation } from '../../components'
-import { GET_USERS } from '../../constants'
+import { GET_USERS, ROLES, UPDATE_USER } from '../../constants'
+
 import './users.scss'
 
 interface User {
@@ -13,20 +15,28 @@ interface User {
   _id: string
   updateAt: Date
   roles: []
+  isUser: boolean
 }
 
-type State = {
+type Users = {
   users: {
     users: [User]
     isUsers: boolean
   }
 }
 
+interface UpdateUser {
+  user: User
+}
 
 export default function Users() {
   const dispatch = useDispatch()
-  const users = useSelector((state: State) => state.users.users)
-  const isUsers = useSelector((state: State) => state.users.isUsers)
+  const users = useSelector((state: Users) => state.users.users)
+  const isUsers = useSelector((state: Users) => state.users.isUsers)
+  const [isShow, setIsShow] = useState(false)
+  const [checkedList, setCheckedList] = useState([])
+  const isUser = useSelector((state: UpdateUser) => state.user.isUser)
+  const [userInfo, setUserInfo] = useState({ _id: '', roles: [], nickName: '', })
 
   useEffect(() => {
     isUsers && Taro.showLoading({ title: '加载中' })
@@ -38,13 +48,55 @@ export default function Users() {
       dispatch({
         type: GET_USERS,
       })
-    } catch (err) { console.log('getUsers ERR -> pages/users', err) }
+    } catch (err) {
+      console.log('getUsers ERR -> pages/users', err)
+    }
   }, [dispatch])
 
   const onImageClick = (avatar) => {
     Taro.previewImage({
       urls: [avatar],
     })
+  }
+
+  const handleOp = (user) => {
+    const roles = user.roles ? user.roles : [];
+    setCheckedList(roles) // 权限勾选状态
+    setIsShow(true) // 显示 modal
+    setUserInfo(user) // 当前选中的用户信息
+  }
+
+  const handleChange = (value) => {
+    setCheckedList(value)
+  }
+
+  const handleClose = () => {
+    if (!isEmpty(checkedList)) {
+      setCheckedList([])
+    }
+    setIsShow(false)
+  }
+
+  const handleConfirm = index => {
+    if (index) {
+      const roles = userInfo.roles ? userInfo.roles : [];
+      if (!isEqual(roles, checkedList)) {
+        // 修改权限后进行更新
+        try {
+          // 更新用户权限
+          dispatch({
+            type: UPDATE_USER,
+            payload: {
+              userData: { roles: checkedList },
+              userId: userInfo._id,
+            }
+          })
+        } catch (err) {
+          throw new Error(`updateUser ERR -> handleConfirm ${err}`)
+        }
+      }
+    }
+    handleClose()
   }
 
   return (
@@ -62,11 +114,61 @@ export default function Users() {
               <View className='name' style={{ width: pxTransform(500) }}>
                 <ClText textColor='brown' cut align='left'>{item.nickName}</ClText>
               </View>
-              <Operation userInfo={item} />
+              <ClButton size='small' plain shape='round' shadow bgColor='green' plainSize='bold' onClick={() => handleOp(item)}>操作</ClButton>
             </View>
           )
         })
       }
+      <ClModal
+        show={isShow}
+        closeWithShadow
+        close
+        custom
+        onCancel={handleClose}
+        renderTitle={
+          <ClTitleBar
+            title='权限操作'
+            textColor='black'
+            type='sub-title'
+            subTitle={userInfo.nickName ? userInfo.nickName : ''}
+            subTitleColor='cyan'
+          />
+        }
+        renderAction={
+          <ClLayout>
+            <ClFlex justify='around'>
+              <View className='flex-sub'>
+                <ClButton
+                  bgColor='gray'
+                  long
+                  size='large'
+                  onClick={handleClose}
+                >
+                  取消
+                </ClButton>
+              </View>
+              <View className='flex-sub'>
+                <ClButton
+                  bgColor='green'
+                  long
+                  size='large'
+                  onClick={handleConfirm}
+                  loading={isUser}
+                >
+                  确认
+                </ClButton>
+              </View>
+            </ClFlex>
+          </ClLayout>
+        }
+      >
+        <AtCheckbox
+          options={ROLES}
+          selectedList={checkedList}
+          onChange={handleChange}
+        ></AtCheckbox>
+      </ClModal>
+      <AtMessage />
     </View>
   )
 }
