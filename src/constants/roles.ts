7068989c7ae701e948308da1f19bcb9552bdec8e
AT <734237292@qq.com>
2020-05-15 15:52:54 +0800

    feat(roles.ts): 权限常量

--- src/constants/roles.ts
+++ src/constants/roles.ts
@@ -3,5 +3,5 @@ export const ROLES = [
 	{ value: '0', label: '发表文章', desc: '发表文章权限' },
 	{ value: '1', label: '用户列表', desc: '用户列表入口显示权限' },
 	{ value: '2', label: '权限设置', desc: '设置用户权限' },
-	{ value: '3', label: '用户删除', desc: '用户删除权限' },
+	{ value: '3', label: '用户删除', desc: '用户删除权限(功能开发中）' },
 ];
