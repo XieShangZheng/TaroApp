9a96f761fddeaf1d4fd24b355d20474e78ead927
AT <734237292@qq.com>
2020-05-20 11:02:18 +0800

    feat(components/index): 移除不存在的引入

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,15 +1,11 @@
 import PostCard from './PostCard';
-import PostForm from './PostForm';
 import Footer from './Footer';
 import Header from './Header';
 import List from './List';
-import Operation from './Operation';
 
 export {
   PostCard,
-  PostForm,
   Footer,
   Header,
   List,
-  Operation,
 };
